fcb2178bbf4023a8f6131765cd894e22d85aeffe
Samir <79602838+the-samir@users.noreply.github.com>
2026-05-07 15:03:04 +0400

    Update create-task.js

--- api/create-task.js
+++ api/create-task.js
@@ -69,7 +69,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         parent: { database_id: '1df0b457c8b48086b007e96a116faf27' },
-        icon: { type: 'external', external: { url: 'https://www.notion.so/icons/checklist_lightgray.svg' } },
+        icon: { type: 'external', external: { url: 'https://www.notion.so/icons/checkmark-verification_white.svg' } },
         properties
       })
     });
